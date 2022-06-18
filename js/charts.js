62a2a10d0fc6dc28ceb2562bf64e4784757ffa5c
Bethany Knowlton <bknowlton22@gmail.com>
2022-06-18 14:03:06 -0700

    correct error

--- js/charts.js
+++ js/charts.js
@@ -1,9 +1,9 @@
 function init() {
   // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+  var selector = d3.select("./data/samples.json");
 
   // Use the list of sample names to populate the select options
-  d3.json("data/samples.json").then((data) => {
+  d3.json("./data/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("data/samples.json").then((data) => {
+  d3.json("./data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -59,7 +59,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("data/samples.json").then((data) => {
+  d3.json("./data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
